74f2edc0ca9da5c399798de481cc7277f931cd54
Manish Rathi <manishrathi19902013@gmail.com>
2019-02-09 20:18:18 +0530

    Initial commit of SwiftQrCodeScanner project.

---
+++ Source/SwiftQrCodeScanner/SwiftQrCodeScanner/SwiftQrCodeScanner.h
@@ -0,0 +1,19 @@
+//
+//  SwiftQrCodeScanner.h
+//  SwiftQrCodeScanner
+//
+//  Created by Manish Rathi on 09/02/19.
+//  Copyright © 2019 Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftQrCodeScanner.
+FOUNDATION_EXPORT double SwiftQrCodeScannerVersionNumber;
+
+//! Project version string for SwiftQrCodeScanner.
+FOUNDATION_EXPORT const unsigned char SwiftQrCodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftQrCodeScanner/PublicHeader.h>
+
+
